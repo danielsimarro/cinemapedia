571f24caa8d5d00e77c9e1ac0840281757287bdb
danielsimarro <danisimarroreigada@gmail.com>
2023-11-06 19:52:40 +0100

    Creacion_appbar

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,5 +1,5 @@
-import 'package:cinemapedia/config/constants/environment.dart';
 import 'package:cinemapedia/presentation/screens/provider/providers.dart';
+import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -34,13 +34,18 @@ class _HomeViewState extends ConsumerState<_HomeView> {
 
   @override
   Widget build(BuildContext context) {
+    // Esto sirve para llamar a la funcion del provider y que nos devuelva la lista de peliculas
     final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
 
-    return ListView.builder(
-        itemCount: nowPlayingMovies.length,
-        itemBuilder: (context, index) {
-          final movie = nowPlayingMovies[index];
-          return ListTile(title: Text(movie.title));
-        });
+    return Column(children: [
+      const CustomAppbar(),
+      Expanded(
+          child: ListView.builder(
+              itemCount: nowPlayingMovies.length,
+              itemBuilder: (context, index) {
+                final movie = nowPlayingMovies[index];
+                return ListTile(title: Text(movie.title));
+              }))
+    ]);
   }
 }
